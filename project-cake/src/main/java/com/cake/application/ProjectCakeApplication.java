c2441b7b195edba54da04c4648b0e6bbb3638962
Sonho Encantado <sonhoencantado012024@gmail.com>
2024-10-24 22:31:09 +0100

    [CAKE] 001 setup

---
+++ project-cake/src/main/java/com/cake/application/ProjectCakeApplication.java
@@ -0,0 +1,13 @@
+package com.cake.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCakeApplication.class, args);
+	}
+
+}
---
+++ project-cake/src/test/java/com/cake/application/ProjectCakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cake.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectCakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
