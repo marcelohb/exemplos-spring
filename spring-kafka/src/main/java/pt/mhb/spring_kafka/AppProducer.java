6d7be0081b5f1a34cdeb03a996ab1c2139dcc915
marcelohb <marcelobattaglion@bol.com.br>
2026-03-26 12:03:00 +0000

    [SPRING-KAFKA] rename the application class

--- spring-kafka/src/main/java/pt/mhb/spring_kafka/AppProducer.java
+++ spring-kafka/src/main/java/pt/mhb/spring_kafka/AppProducer.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringKafkaApplication {
+public class AppProducer {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringKafkaApplication.class, args);
+		SpringApplication.run(AppProducer.class, args);
 	}
 
 }
