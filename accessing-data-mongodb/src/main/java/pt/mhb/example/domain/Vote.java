5cfd327476b04760b9efa293bba03c8fee934a69
marcelohb <marcelobattaglion@bol.com.br>
2026-04-21 13:44:43 +0100

    improved mongo collection

--- accessing-data-mongodb/src/main/java/pt/mhb/example/domain/Vote.java
+++ accessing-data-mongodb/src/main/java/pt/mhb/example/domain/Vote.java
@@ -1,14 +1,25 @@
 package pt.mhb.example.domain;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
+@Document(collection = "votes")
 public class Vote {
 
     @Id
-    private String id;
+    private ObjectId id;
+
+    @CreatedDate
     private final LocalDateTime createdAt;
+
+    @Min(value = 1, message = "Score must be 1,2 or 3")
+    @Max(value = 3, message = "Score must be 1,2 or 3")
     private final int score;
 
     public Vote(int score) {
